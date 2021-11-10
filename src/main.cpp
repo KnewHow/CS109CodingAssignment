2dc10636159dc6dbbe16e2482a12d8f0976ca882
Knewhow <how.yuangh@gmail.com>
2021-11-10 10:07:28 +0800

    complete

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,11 @@ inline bool stringToBool(const std::string &boolStr);
 float calculateProbT();
 std::array<float, 5> calculateProbG();
 float calculateProbGI(int index);
+std::array<bool, 5> checkTIsIndependentWithG();
+bool checkTIsIndependentWithGI(int index);
+bool compare_float(float v1, float v2);
+float calculateEK();
+int calculateGeneExpress(int index);
 
 int main(int argc, char** argv) {
     if(argc < 1) {
@@ -25,9 +30,61 @@ int main(int argc, char** argv) {
     }
     std::string filepath = std::string(argv[1]);
     readBatsFromFile(filepath);
-    calculateProbT();
-    calculateProbG();
+    // calculateProbT();
+    // calculateProbG();
+    checkTIsIndependentWithG();
+    //calculateEK();
     return 0;
+}
+
+float calculateEK() {
+    float expectation = 0;
+    for(int i = 0; i < 5; ++i) {
+        float p_GI = calculateProbGI(i + 1);
+        int expressed = calculateGeneExpress(i + 1);
+        expectation += p_GI * expressed;
+    }
+    std::cout << std::fixed << std::setprecision(std::numeric_limits<float>::digits10) << "EK is: " << expectation << std::endl;
+    return expectation;
+}
+
+int calculateGeneExpress(int index) {
+    int expressed = 0;
+    for(int i = 0; i < bats.size(); ++i)
+        if(bats[i][index])
+            ++expressed;
+    return expressed;
+}
+
+std::array<bool, 5> checkTIsIndependentWithG() {
+    std::array<bool, 5> rs;
+    for(int i = 0; i < 5; ++i)
+        rs[i] = checkTIsIndependentWithGI(i + 1);
+    return rs;
+}
+
+bool checkTIsIndependentWithGI(int index) {
+    int TandGISum = 0;
+    for(int i = 0; i < bats.size(); ++i) {
+        if(bats[i][index] && bats[i].withEbola)
+            ++TandGISum;
+    }
+    float p_GI = calculateProbGI(index);
+    float p_TandGI = (float)TandGISum / bats.size();
+    float p_T = calculateProbT();
+    float p_Tbefore_GI = p_TandGI / p_GI;
+    bool r = compare_float(p_Tbefore_GI, p_T);
+    std::cout << std::fixed << std::setprecision(std::numeric_limits<float>::digits10) << "P(T) is: " << p_T << std::endl;
+    std::cout << std::fixed << std::setprecision(std::numeric_limits<float>::digits10) << "P(G_" << index << ") is:" << p_GI << ", P(T and G_" << index << ") is: " << p_TandGI << ", P(T | G_" << index << ") is: " << p_Tbefore_GI << ", they are independent: " << std::boolalpha << r << std::endl;
+    return r;
+}
+
+bool compare_float(float v1, float v2) {
+    float ShadowEpsilon = 0.0001f;
+    if(std::abs(v1 - v2) < ShadowEpsilon)
+        return true;
+    else 
+        return false;
 }
 
 float calculateProbT() {
@@ -37,7 +94,6 @@ float calculateProbT() {
         if(bats[i].withEbola) ++withEbola;
     
     float p = (float)withEbola / totol;
-    std::cout << "P(T) is: " << std::fixed << std::setprecision(std::numeric_limits<float>::digits10) << p << std::endl;
     return p;
 }
 
